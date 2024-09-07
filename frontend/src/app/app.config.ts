690cd062eb8cd9e5969e352b7a9567d7ce5f1588
Diego Lima <xxdiegolsxx@gmail.com>
2024-09-07 17:10:43 -0300

    refactor: :recycle: imports
    app.config
    
    removido alguns imports não necessários

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,20 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { AppRoutingModule, routes } from './app.routes';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
-    CommonModule,
-    AppRoutingModule,
   ],
 };
